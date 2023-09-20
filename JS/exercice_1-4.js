8d38ede19ec2ba230de04307fdb5c2876e8216e7
RobinBayraktar <nurhayat.bayraktar@student.pxl.be>
2023-09-20 10:40:53 +0200

    finished exercice 4

---
+++ JS/exercice_1-4.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const sentence1 = "I'm a sentence!";
+const sentence2 = "I'm a sentence too!";
+const sentence3 = "I guess I'm a sentence as well";
+
+console.log(sentence1);
+console.log(sentence2);
+console.log(sentence3);
---
+++ JS/exercice_1-5.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const sentence1 = "I'm a sentence!";
+const sentence2 = "I'm a sentence too!";
+const sentence3 = "I guess I'm a sentence as well";
+
+console.log(sentence1 + " " + sentence2 +  " " + sentence3);
