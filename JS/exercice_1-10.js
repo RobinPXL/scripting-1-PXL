0189864e6fd28bde06a7fed81fec85a2e62dd572
RobinBayraktar <nurhayat.bayraktar@student.pxl.be>
2023-09-20 20:20:11 +0200

    finished ex 10

--- JS/exercice_1-10.js
+++ JS/exercice_1-10.js
@@ -1,1 +1,11 @@
 "use strict";
+
+//ask two random numbers
+let randomNumber1 = prompt("Give me a random number! (don't add comma's, dividers or anything, just a straight number)", 5);
+let randomNumber2 = prompt("Give me another random number, same principle as before!", 3);
+
+//multiply numbers
+let multNumber = randomNumber1 * randomNumber2;
+
+//output multiplication on webpage
+document.getElementById("change").innerHTML = `This is the new multiplication number: ${multNumber}.`;
