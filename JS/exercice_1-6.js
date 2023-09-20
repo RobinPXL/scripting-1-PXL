87dc70a671639221bf96586209acdf0d4f353022
RobinBayraktar <nurhayat.bayraktar@student.pxl.be>
2023-09-20 19:32:50 +0200

    finished exercice 1.6

--- JS/exercice_1-6.js
+++ JS/exercice_1-6.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const variable = "Ik wil een koekje";
+document.getElementById("change").innerHTML = variable;
+
+//Welk karakter staat op positie 0?
+console.log(variable[0]);
+
+//Welk karakter staat op positie 3?
+console.log(variable[3]);
+
+//Op welke positie staat karakter ‘k’ voor de laatste keer?
+console.log(variable.lastIndexOf("k"));
+
+//Op welke positie staat karakter ‘e’ voor de laatste keer?
+console.log(variable.lastIndexOf("e"));
+
+//Uit hoeveel karakters bestaat de stringwaarde?
+console.log(variable.length);
