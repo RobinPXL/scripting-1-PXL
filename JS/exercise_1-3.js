bf5205b7e2a05519957df40fcff253c58b65b173
RobinBayraktar <nurhayat.bayraktar@student.pxl.be>
2023-09-20 10:24:59 +0200

    finished exercice 3

--- JS/exercise_1-3.js
+++ JS/exercise_1-3.js
@@ -1,15 +1,18 @@
-"use strict";
+const random_number = prompt("Please select a number between 1-5", 3);
+console.log(random_number);
 
-let random_number = prompt("Please select a number between 1-5", 5);
+if (parseInt(random_number) == null || parseInt(random_number) == 0) {
 
-if (random_number == null || random_number == 0) {
   document.getElementById("answer").innerHTML = "You can't add nothing/0!";
-} else if(random_number < 5){
+
+} else if(parseInt(random_number) > 5){
+
     document.getElementById("answer").innerHTML = "Your number can't be greater than 5!";
+
 } else{
+
     document.getElementById("random_number").innerHTML = random_number;
 };
-
 
 
 
