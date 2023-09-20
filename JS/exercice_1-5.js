83d5cd99fbc25427369825279b4f521379449035
RobinBayraktar <nurhayat.bayraktar@student.pxl.be>
2023-09-20 19:03:04 +0200

    finished exercice 1.5

--- JS/exercice_1-5.js
+++ JS/exercice_1-5.js
@@ -1,16 +1,12 @@
 "use strict";
-
-// Console log versie
-
 const sentence1 = "I'm a sentence!";
 const sentence2 = "I'm a sentence too!";
 const sentence3 = "I guess I'm a sentence as well.";
 
-console.log(sentence1 + " " + sentence2 +  " " + sentence3);
+// Console log versie
+const longSentence = sentence1 + " " + sentence2 +  " " + sentence3;
+console.log(longSentence);
 
 // Webpage/HTML versie
-
-let document = document.getElementById("change");
-
-document.innerHTML = "";
-document.innerHTML = null;
+let change = document.getElementById("change");
+change.innerHTML = longSentence;
