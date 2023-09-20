f83097d4f1c6efe3db214efe71366c26fe30658e
RobinBayraktar <nurhayat.bayraktar@student.pxl.be>
2023-09-20 19:59:20 +0200

    working on ex 1.9

---
+++ JS/exercice_1-9.js
@@ -0,0 +1,15 @@
+"user strict";
+
+"use strict";
+
+//asking for random word to user
+let randomSentence = prompt("Give a random sentence!", "The fox jumps over the rainbow");
+console.log("This was your random sentence: " + randomSentence);
+
+if(randomSentence.includes(" ")){
+    
+}
+//show the length of the random word back to the user
+const answer = "This was the length of your random word: " + randomWord.length;
+alert(answer);
+console.log(answer);
