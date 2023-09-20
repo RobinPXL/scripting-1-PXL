f499eb9b728954a800fa71fa7cdffef22343d363
RobinBayraktar <nurhayat.bayraktar@student.pxl.be>
2023-09-20 20:29:44 +0200

    finished ex 11

---
+++ JS/exercice_1-11.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//ask two random numbers
+const randomNumber1 = prompt("Give me a random number! (don't add comma's, dividers or anything, just a straight number)", 5);
+const randomNumber2 = prompt("Give me another random number, same principle as before!", 3);
+
+//multiply numbers
+const multNumber = randomNumber1 * randomNumber2;
+
+const sumNumber = parseInt(randomNumber1) + parseInt(randomNumber2);
+
+const subtractNumber = randomNumber1 - randomNumber2;
+
+const divisionNumber = randomNumber1 / randomNumber2;
+
+//output multiplication on webpage
+document.getElementById("change").innerHTML = `This is the new multiplication number: ${multNumber}.`;
+
+document.getElementById("change2").innerHTML = `This is the new sum: ${sumNumber}.`;
+
+document.getElementById("change3").innerHTML = `This is the new subtraction: ${subtractNumber}.`;
+
+document.getElementById ("change4").innerHTML = `This is the new division: ${divisionNumber}.`;
