5da84eb50fd7782c9208a520264af0a16fa93a65
RobinBayraktar <nurhayat.bayraktar@student.pxl.be>
2023-09-20 19:16:30 +0200

    updated CSS files, change in HTML and finished ex 1.4

--- JS/exercice_1-4.js
+++ JS/exercice_1-4.js
@@ -7,3 +7,7 @@ const sentence3 = "I guess I'm a sentence as well.";
 console.log(sentence1);
 console.log(sentence2);
 console.log(sentence3);
+
+document.getElementById("change").innerHTML= sentence1;
+document.getElementById("change2").innerHTML = sentence2;
+document.getElementById("change3").innerHTML = sentence3;
