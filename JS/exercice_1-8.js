93e1e0315e256d6be147e07cd7bbedfac9cb5f29
RobinBayraktar <nurhayat.bayraktar@student.pxl.be>
2023-09-20 19:47:15 +0200

    finished exercice 1.8

---
+++ JS/exercice_1-8.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//asking for random word to user
+let randomWord = prompt("Give a random word!", "jaguar");
+console.log("This was your random word: " + randomWord);
+
+
+//show the length of the random word back to the user
+const answer = "This was the length of your random word: " + randomWord.length;
+alert(answer);
+console.log(answer);
