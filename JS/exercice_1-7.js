9c85c076bf0fac89e34a6e56cea917c2dfed6ddf
RobinBayraktar <nurhayat.bayraktar@student.pxl.be>
2023-09-20 19:37:38 +0200

    finished exercice 1.7

---
+++ JS/exercice_1-7.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const variable = "Ik wil een lasagne van PXL-catering";
+const updateVariable = variable.toUpperCase();
+
+//on console
+console.log(updateVariable);
+
+//on web
+document.getElementById("change").innerHTML = updateVariable;
